adf443c842e103d2fad2a9d431137802758cdda3
Mursalin-Dowla <justjim844@gmail.com>
2023-01-18 10:13:26 +0600

    Contact page demo added+end for now

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,11 +1,106 @@
-import React from 'react';
+import React from "react";
 
 const AboutUs = () => {
-    return (
-        <div className='min-h-screen mt-20 ml-10'>
-       
-       </div>
-    );
+  return (
+    <div className="min-h-screen mt-20 ml-10">
+      <form class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-first-name"
+            >
+              First Name
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="First Name"
+            />
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-last-name"
+            >
+              Last Name
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Last Name"
+            />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              Message
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="message"
+            />
+          </div>
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              Contact Number
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Type Number"
+            />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-2">
+          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-city"
+            >
+              City
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-city"
+              type="text"
+              placeholder="Maheshpur"
+            />
+          </div>
+
+          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-zip"
+            >
+              Zip
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-zip"
+              type="text"
+              placeholder="7340"
+            />
+          </div>
+        </div>
+      </form>
+      <button className="w-[250px] h-[50px] bg-[#025] hover:bg-[#f80] mt-10 font-serif font-semibold text-[#ff0000] hover:text-white rounded-md text-lg">
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,35 @@
-import React, { useContext } from 'react';
-import { MyContext } from '../../App';
-import CartItem from '../CartItem/CartItem';
+import React, { useContext } from "react";
+import { MyContext } from "../../App";
+import CartItem from "../CartItem/CartItem";
 
 const Cart = () => {
-    const myCart = useContext(MyContext);
-    const {cart, setCart} = myCart;
-    const handleRemoveFromCart = (selectedItem) =>{
-        const rest = cart.filter(cartItem=>cartItem.id !== selectedItem.id)
-        setCart(rest)
-    }
+  const myCart = useContext(MyContext);
+  const { cart, setCart } = myCart;
+  const handleRemoveFromCart = (selectedItem) => {
+    const rest = cart.filter((cartItem) => cartItem.id !== selectedItem.id);
+    setCart(rest);
+  };
 
-    return (
-        <div className='md:mt-40 mt-20 min-h-screen md:ml-20 ml-5 md:mr-20 mr-5'>
-            {
-            cart.map((cartItem)=><CartItem key={cartItem.id}
-            cartItem={cartItem}
-            handleRemoveFromCart={handleRemoveFromCart}
-            />)
-            }
-            <button className={cart.length > 0 ? 'w-[250px] h-[50px] bg-[#025] hover:bg-[#f80] mt-10 font-serif font-semibold text-[#ff0000] hover:text-white rounded-md text-lg': 'hidden'}>Proceed To payment</button>
-        </div>
-    );
+  return (
+    <div className="md:mt-40 mt-20 min-h-screen md:ml-20 ml-5 md:mr-20 mr-5">
+      {cart.map((cartItem) => (
+        <CartItem
+          key={cartItem.id}
+          cartItem={cartItem}
+          handleRemoveFromCart={handleRemoveFromCart}
+        />
+      ))}
+      <button
+        className={
+          cart.length > 0
+            ? "w-[250px] h-[50px] bg-[#025] hover:bg-[#f80] mt-10 font-serif font-semibold text-[#ff0000] hover:text-white rounded-md text-lg"
+            : "hidden"
+        }
+      >
+        Proceed To payment
+      </button>
+    </div>
+  );
 };
 
 export default Cart;
